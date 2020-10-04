286adbd3604c68d4e63dc8a7f30cf35d0bb6f0ed
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-04 23:53:05 +0200

    Update

--- api/new.js
+++ api/new.js
@@ -1,9 +1,40 @@
-export default (req, res) => {
-  console.log(req.headers.host);
+const {AIRTABLE_BASE, AIRTABLE_KEY} = process.env;
+
+const Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: AIRTABLE_KEY
+});
+const base = Airtable.base(AIRTABLE_BASE);
+
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  
+  const {title, message, url, error} = req.query;
+  base('Errors').create([
+    {
+      "fields": {
+        "title": title,
+        "message": message,
+        "url": url,
+        "error": error,
+        "status": "Todo"
+      }
+    }
+  ], (err, recs) => {
     res.json({
-      body: req.body,
       query: req.query,
       cookies: req.cookies,
-      res: req.headers['x-forwarded-host']
+      error: err,
     })
+  })  
+  
+  
 }
+// http://localhost:3000/api/new?title=This is a test&message=something went wrong when adding record&url=root&error=official error message
