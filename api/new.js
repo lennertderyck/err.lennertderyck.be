13ea33d2183709298753590b2a799e26201f6b51
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-04 23:03:56 +0200

    Update

--- api/new.js
+++ api/new.js
@@ -1,7 +1,9 @@
 export default (req, res) => {
+  console.log(req.headers.host);
     res.json({
       body: req.body,
       query: req.query,
       cookies: req.cookies,
+      res: req.headers['x-forwarded-host']
     })
 }
